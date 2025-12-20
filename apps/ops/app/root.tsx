ed84c43e0eb84f2038b8bc0d65b6377d7c9bf5f6
Jiajian Yang <jiajian.yang@searchspring.com>
2025-12-20 00:13:32 -0500

    fix: fixed layout structure in App component for better flexibility and responsiveness

--- apps/ops/app/root.tsx
+++ apps/ops/app/root.tsx
@@ -96,8 +96,10 @@ export default function App() {
           <SiteSwitcher currentSite={siteId || ''} availableSites={accessibleSites || []} />
         </div>
       </div>
-      <main className="flex flex-1 min-h-0">
-        <Outlet context={{ siteId, accessibleSites }} />
+      <main className="flex-1 min-h-0">
+        <div className="flex h-full min-h-0 flex-col">
+          <Outlet context={{ siteId, accessibleSites }} />
+        </div>
       </main>
     </div>
   );
